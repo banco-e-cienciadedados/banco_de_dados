ece1e865bbdd54530acd026fb49d0df6efa1e946
Pedro <pedrohenriquedias966@icloud.com>
2025-06-16 12:09:07 -0300

    ✨ feat: update_dml

--- DML.sql
+++ DML.sql
@@ -16,23 +16,23 @@ INSERT INTO gerenciamento.filmes (titulo, disponivel, ano, categoria) VALUES
 ('Matrix', TRUE, 1999, 'Ficção Científica');
 
 INSERT INTO gerenciamento.locacao (data_inicio, data_final, valor, id_cliente, id_filme, id_funcionario, id_endereço) VALUES
-('2025-05-16', '2025-05-20', 15.00, 1, 1, 1, 1), -- Cliente 1 aluga "O Senhor dos Anéis" com funcionário 1
-('2025-05-16', '2025-05-18', 12.00, 2, 2, 2, 2); -- Cliente 2 aluga "Matrix" com funcionário 2
+('2025-05-16', '2025-05-20', 15.00, 1, 1, 1, 1),    -- Cliente 1 aluga "O Senhor dos Anéis" com funcionário 1
+('2025-05-16', '2025-05-18', 12.00, 2, 2, 2, 2);    -- Cliente 2 aluga "Matrix" com funcionário 2
 
 -- Atualizar o valor da locação do ID 2
 UPDATE gerenciamento.locacao
 SET valor = 14.00
 WHERE id_locacao = 2;
 
--- Atualizar a disponibilidade de um filme (por exemplo, o filme foi devolvido)
+
 UPDATE gerenciamento.filmes
-SET disponivel = TRUE
+SET disponivel = TRUE             -- Atualizar a disponibilidade de um filme (por exemplo, o filme foi devolvido)
 WHERE id_filme = 2;
 
--- Remover um cliente específico
+
 DELETE FROM gerenciamento.clientes
 WHERE id_cliente = 2;
 
--- Remover uma locação
+
 DELETE FROM gerenciamento.locacao
 WHERE id_locacao = 1;
